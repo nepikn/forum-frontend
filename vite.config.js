8db31ecd26349035fbba0166b3cdac2a9a49f07e
nepikn <31944706+nepikn@users.noreply.github.com>
2024-05-15 19:12:58 +0800

    init auth html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import path from "node:path";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
 
-  return { base: `/${env.VITE_GITHUB_REPO}` };
+  return {
+    // base: `/${env.VITE_GITHUB_REPO}`,
+    build: {
+      rollupOptions: {
+        input: { main: path.resolve(process.cwd(), "index.html") },
+        auth: { main: path.resolve(process.cwd(), "auth/index.html") },
+      },
+    },
+  };
 });
