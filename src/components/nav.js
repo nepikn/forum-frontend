6fb76fdefb64c36b69ad215cf4f3d4db0a526fcf
nepikn <31944706+nepikn@users.noreply.github.com>
2024-07-13 17:01:15 +0800

    fix: auth href

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,6 +7,8 @@ export default async function Nav({ state: { userName }, handler: { user } }) {
   nav.querySelector(".userName").textContent = userName;
   if (userName === null) {
     nav.querySelector("[data-user=true]").remove();
+    nav.querySelector("#auth").href =
+      (import.meta.env.VITE_BASE ?? "") + "/auth/";
   } else {
     nav.querySelector("[data-user=false]").remove();
     nav.querySelector(".logout").onclick = user.logOut;
