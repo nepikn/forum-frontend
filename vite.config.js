00e5cd8d0811d3bf6abe01a9fabbad7442e42d73
nepikn <31944706+nepikn@users.noreply.github.com>
2024-07-13 14:40:08 +0800

    fix(env): base dir

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,18 @@
 import path from "node:path";
 import { defineConfig, loadEnv } from "vite";
 
-export default {
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(process.cwd(), "index.html"),
-        auth: path.resolve(process.cwd(), "auth/index.html"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE,
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(process.cwd(), "index.html"),
+          auth: path.resolve(process.cwd(), "auth/index.html"),
+        },
       },
     },
-  },
-};
-
-// export default defineConfig(({ command, mode }) => {
-//   const env = loadEnv(mode, process.cwd());
-
-//   return {
-//     // base: `/${env.VITE_GITHUB_REPO}`,
-//     build: {
-//       rollupOptions: {
-//         input: { main: path.resolve(process.cwd(), "index.html") },
-//         auth: { main: path.resolve(process.cwd(), "auth/index.html") },
-//       },
-//     },
-//   };
-// });
+  };
+});
